564bd585df703eed9d3a4abc55317d6625c18eaa
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2016-02-10 11:31:47 +0200

    added custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'register' , to: 'devise/registrations#new' ,as: :register 
+    get 'login' , to: 'devise/sessions#new' ,as: :login 
+    get 'logout' , to: 'devise/sessions#destroy' ,as: :logout 
+  end
+
+  get 'feed',to:'statuses#index',as: :feed 
+
   resources :statuses
   root 'statuses#index'
   # The priority is based upon order of creation: first created -> highest priority.
